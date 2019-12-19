f8bd3ddd36ddd9a8ca3b8cb47faa00a6e6789fc8
hyodofumiya <f.hyodo.0123@gmail.com>
2019-12-19 19:33:50 +0900

    わけわからん

--- db/migrate/20191216100913_create_user_groups.rb
+++ db/migrate/20191216100913_create_user_groups.rb
@@ -1,9 +1,9 @@
 class CreateUserGroups < ActiveRecord::Migration[5.0]
   def change
-    create_table :user_groups do |t|
+    create_table :users_group do |t|
 
-      t.integer     :user_id, null: false, foreign_key: true
-      t.integer     :group_id, null: false, foreign_key: true
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
