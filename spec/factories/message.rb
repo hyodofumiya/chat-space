ca3ba35ba465664fff50d73864aef8a07ed05f03
hyodofumiya <f.hyodo.0123@gmail.com>
2020-01-11 12:44:00 +0900

    テスト

--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :message do
     tweet {Faker::Coffee.blend_name}
     image {File.open("#{Rails.root}/public/images/猫画像１.jpeg")}
-    user_id
-    group_id
+    user_id {1}
+    group_id {1}
   end
 end
