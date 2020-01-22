66c72bc86a8870556c8817d90c79aee3ed5b7dc6
hyodofumiya <f.hyodo.0123@gmail.com>
2020-01-22 10:20:48 +0900

    インクリメンタルサーチ機能実装完了
    インクリメンタルサーチ機能の実装を完了した。

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -17,6 +17,21 @@ $(function(){
             `;
             $("#user-search-result").append(html);
   }
+
+  function addDeleteUser(id, name) {
+    let html =`
+              <div class="chat-group-user clearfix" id="${id}">
+                <p class="chat-group-user__name">${name}</p>
+                <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+              </div>`;
+    $(".js-add-user").append(html);
+  }
+
+  function addMember(userId) {
+    let html =`<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    $(`#${userId}`).append(html);
+  }
+
   $('.chat-group-form__input').on("keyup", function() {
     let input = $("#user-search_field").val();
     $.ajax({
@@ -39,7 +54,19 @@ $(function(){
         }
       })
       .fail(function() {
-        alert("通信エラーです。ユーザーが表示されません。");
+        alert("ユーザー検索に失敗しました");
       });
+  });
+
+  $('#user-search-result').on("click",'.user-search-add', function() {
+    const userId = $(this).attr('data-user-id');
+    const userName = $(this).attr('data-user-name');
+    $(this).parent().remove();
+    addDeleteUser(userId, userName);
+    addMember(userId);
+    });
+
+  $(document).on("click", '.js-remove-btn', function() {
+    $(this).parent().remove();
   });
 });
