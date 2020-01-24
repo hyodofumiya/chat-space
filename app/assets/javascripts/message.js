8520a0fb1a087637c221de6e7baac299b22ee47f
hyodofumiya <f.hyodo.0123@gmail.com>
2020-01-24 20:59:14 +0900

    同期通信完了（自己）

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,84 @@
 $(function(){ 
+  function reloadMessages() {
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !==0 ) {
+        var insertHTML ='';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chat-main__tweet-lists').append(insertHTML);
+        $('.chat-main__tweet-lists').animate({ scrollTop: $('.chat-main__tweet-lists')[0].scrollHeight});
+        $('form')[0].reset();
+        $('.new_message__submit-btn').prop('disabled', false);
+      }
+    })
+    .fail(function(){
+      console.log("error");
+    });
+  };
+
   function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="message__info">
-           <div class="message__info__user-name">
-             ${message.user_name}
-           </div>
-           <div class="message__info__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="messge__value">
-           <p class="message__value__tweet">
-             ${message.tweet}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="message__info">
-           <div class="message__info__user-name">
-             ${message.user_name}
-           </div>
-           <div class="message__info__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="message__value">
-           <p class="message__value__tweet">
-             ${message.tweet}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
- }
+    if ( message.image && message.tweet ) {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="message__info">
+            <div class="message__info__user-name">
+              ${message.user_name}
+            </div>
+            <div class="message__info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message__value">
+            <p class="message__value__tweet">
+              ${message.tweet}
+            </p>
+            <img src=${message.image} class="message__value__image" >
+          </div>
+        </div>`
+    } else if (message.tweet) {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="message__info">
+            <div class="message__info__user-name">
+              ${message.user_name}
+            </div>
+            <div class="message__info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message__value">
+            <p class="message__value__tweet">
+              ${message.tweet}
+            </p>
+          </div>
+        </div>`
+    } else if (message.image) {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="message__info">
+            <div class="message__info__user-name">
+              ${message.user_name}
+            </div>
+            <div class="message__info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message__value">
+          <img src=${message.image} class="message__value__image" >
+          </div>
+        </div>`
+    };
+    return html;
+  };
+
 $('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
@@ -63,4 +103,7 @@ $('#new_message').on('submit', function(e){
     $('.new_message__submit-btn').prop('disabled', false);
   });
 })
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
